5b1bef6d73f39f3cea4e4c965fe1e2a40ecddb7f
Evan Duncan <evan.e.duncan@gmail.com>
2021-12-24 00:17:29 -0600

    Add frequency to note converter that works when frequency is greater than base frequency.

---
+++ src/audio_analyzer/note.clj
@@ -0,0 +1,24 @@
+(ns audio-analyzer.note)
+
+(def notes ["A" "A#" "B" "C" "C#" "D" "D#" "E" "F" "F#" "G" "G#"])
+
+(defn- approx-freq? [target steps]
+  (< (Math/abs (- target
+                  (* 55
+                     (Math/pow 2
+                               (+ 3 (/ steps 12))))))
+     5))
+
+(defn- approx-freq [n]
+  (let [a (Math/pow 2 1/12)
+      iterator (if (> n 440) inc dec)]
+  (loop [x 0]
+    (if (approx-freq? n x)
+      x
+      (recur (iterator x))))))
+
+(defn freq->note
+  "Convert a frequency to a musical note of the equal tempered scale"
+  [n]
+  (notes (rem (approx-freq n) 12)))
+
---
+++ test/audio_analyzer/note_test.clj
@@ -0,0 +1,9 @@
+(ns audio-analyzer.note-test
+  (:require  [clojure.test :refer [deftest is testing]]
+             [audio-analyzer.note :refer [freq->note]]))
+
+(deftest frequencies-to-notes
+  (testing "Proper conversion when frequency is higher than base"
+    (is (= (freq->note 440) "A"))
+    (is (= (freq->note 523) "C"))
+    (is (= (freq->note 880) "A"))))
